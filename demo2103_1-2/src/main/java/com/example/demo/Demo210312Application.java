63db28b2ea12e65391433d6ab3551ce7fbf4320d
nicehappy0919 <candysy2014@naver.com>
2021-05-20 12:20:19 +0900

    프로젝트 추가

---
+++ demo2103_1-2/src/main/java/com/example/demo/Demo210312Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo210312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo210312Application.class, args);
+	}
+
+}
